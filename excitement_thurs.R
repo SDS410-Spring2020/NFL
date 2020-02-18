dd49864243558ede699691faf4d0c5d04fa881d5
aballou16 <aballou@smith.edu>
2020-02-18 14:00:16 -0500

    fixed piping for creating excitment df

--- excitement_thurs.R
+++ excitement_thurs.R
@@ -6,18 +6,22 @@ games<- read.csv("data/games_Smith.csv")
 pbp<- read.csv("data/pbp_Smith.csv")
 
 #creating binary thursday in games df
-thurs_games <- games %>% 
+thurs_games <- fulldata_with_thurs %>% 
   mutate(is_thurs = ifelse(Game_Day == "Thursday", 1, 0))
 
 #create an excitement variable
 #using thurs_games data
-excitement <- thurs_games %>% 
+excitement <- fulldata_with_thurs %>% 
   group_by(GameKey) %>% 
   summarise(excite_score = sum(abs(delta_wp_home)))
   
 #join excitement score with thurs_games ds 
 excitement_joined <- excitement %>% 
   full_join(thurs_games, by = "GameKey")
+
+thurs_excite_box <- ggplot(data = excitement_joined, aes(y = rowSums, group = as.factor(have_arthritis), color = as.factor(have_arthritis))) +
+  geom_boxplot() +
+  ggtitle("Side by Side Box plots using rowSums ~ Gender")
 
 #look at excitement score on Thurs vs Not Thurs
 boxplot(excitement_joined[["excite_score"]] ~ excitement_joined[["is_thurs"]], data = excitement_joined,

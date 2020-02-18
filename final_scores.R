42a4d43391265511ad9f0451d82edd92c21f954a
Lizette <lcarpenter@smith.edu>
2020-02-18 13:33:52 -0500

    some edits to final scores and start of visualizing data

--- final_scores.R
+++ final_scores.R
@@ -3,7 +3,6 @@
 # - 
 
 # Data Exploration: 
-# - game key, isThurs
 ---------------------------------------------------------------------
 
 #loading libraries 
@@ -25,10 +24,8 @@ pbp <- pbp %>%
 
 #joining games and pbp (from Elaona)
 full_data <- games %>% 
-  left_join(pbp, by="game_key")
-
-#adding is_thurs variable (from Anna)
-full_data <- full_data %>% 
+  left_join(pbp, by="game_key") %>% 
+  #adding is_thurs variable (from Anna)
   mutate(is_thurs = ifelse(game_day == "Thursday", 1, 0),
          is_thurs = is.logical(is_thurs)) 
 
@@ -46,7 +43,7 @@ final_score_vars <- pbp %>%
          is_scoring_play) 
 
 #checking for na values 
-is.na(final_score)
+#is.na(final_score)
 
 #finds total number of scoring plays in each game (does not differentiate between teams)  
 scoring_plays <- final_score_vars %>%
@@ -61,6 +58,7 @@ scoring_plays <- final_score_vars %>%
 #score if the observation (or play) is a scoring play. Otherwise, observation will be a 0.  
 mutate_score_after_play <- pbp %>% 
   select(game_key, 
+         season, 
          home_club_code, 
          visitor_club_code, 
          is_scoring_play, 
@@ -99,4 +97,37 @@ winner <- final_scores %>%
         home_final < visitor_final ~ as.character(visitor_club_code),
         home_final == visitor_final ~ "tie"), 
       is_tie = ifelse(winning_team == "tie", 1, 0))
-  
+
+#joining both data frames 
+#includes seasons between 2010 and 2019 
+joined <- inner_join(pbp, games, by = NULL, copy = FALSE) 
+
+#full data with final scores 
+data_with_scores <- joined %>% 
+  group_by(game_key, 
+           home_club_code, 
+           visitor_club_code) %>% 
+  mutate( 
+    home_score_after_play = 
+      case_when(
+        is_scoring_play == 0 ~ 0,
+        TRUE ~ as.double(home_score_after_play)),
+    visitor_score_after_play = 
+      case_when(
+        is_scoring_play == 0 ~ 0, 
+        TRUE ~ as.double(visitor_score_after_play))) %>% 
+  filter(!home_score_after_play == 0,
+         !visitor_score_after_play == 0) %>%
+  group_by(game_key) %>%
+  mutate(home_final = max(home_score_after_play),
+         visitor_final = max(visitor_score_after_play),
+         winning_team = 
+           case_when(
+             home_final > visitor_final ~ as.character(home_club_code), 
+             home_final < visitor_final ~ as.character(visitor_club_code),
+             home_final == visitor_final ~ "tie"), 
+         is_tie = ifelse(winning_team == "tie", 1, 0),
+         is_winner = 
+           case_when(
+             winning_team == 
+           ))

698fdad5dbfa0e07ff162a4882855b79ca73b349
aballou16 <aballou@smith.edu>
2020-02-18 14:20:00 -0500

    calculated mean score for thurs vs non thurs

--- excitement_thurs.R
+++ excitement_thurs.R
@@ -1,5 +1,7 @@
 #Use the fulldata_with_thurs ds (thursday = binary)
+#load tidyverse
 head(fulldata_with_thurs)
+library(tidyverse)
 
 #reading in the two nfl data frames 
 games<- read.csv("data/games_Smith.csv")
@@ -18,10 +20,22 @@ excitement_joined <- excitement %>%
 #look at exctiment score on Thurs vs Not Thurs using ggplot
 thurs_excite_box <- ggplot(data = excitement_joined, aes(y = excite_score, group = as.factor(is_thurs), color = as.factor(is_thurs))) +
   geom_boxplot() +
-  ggtitle("Comparing excitement on Thurs. vs. not Thurs games")
+  ggtitle("Comparing excitement on Thurs. vs. not Thurs games") 
 thurs_excite_box
 
 #look at excitement score on Thurs vs Not Thurs
 boxplot(excitement_joined[["excite_score"]] ~ excitement_joined[["is_thurs"]], data = excitement_joined,
         xlab="Thursday/Not Thursday", ylab="excitement score",
         main = "Score Distributions on Thursdays and Non-Thursdays")
+
+#simple statistical analyses: difference in means 
+head(excitement_joined)
+excitement_means <- excitement_joined %>% 
+  select(GameKey, excite_score, is_thurs) %>% 
+  group_by(is_thurs) %>%
+  summarise(mean(excite_score))
+excitement_means
+
+
+
+

34886ea5a82b9479226942cd1073fd7c3e203fde
aballou16 <aballou@smith.edu>
2020-02-17 21:54:07 -0500

    added side by side boxplots

--- excitement_thurs.R
+++ excitement_thurs.R
@@ -1,14 +1,22 @@
-#Use the fulldata_with_thurs ds (thursday = binary)
-head(fulldata_with_thurs)
+#Use the games ds (thursday = binary)
+head(games)
+
+
+#creating binary thursday in games df
+thurs_games <- games %>% 
+  mutate(is_thurs = ifelse(Game_Day == "Thursday", 1, 0))
 
 #create an excitement variable
-#using fulldata_with_thurs data
-
-excitement <- fulldata_with_thurs %>% 
+#using thurs_games data
+excitement <- thurs_games %>% 
   group_by(GameKey) %>% 
   summarise(excite_score = sum(abs(delta_wp_home)))
   
+#join excitement score with thurs_games ds 
+excitement_joined <- excitement %>% 
+  full_join(thurs_games, by = "GameKey")
+
 #look at excitement score on Thurs vs Not Thurs
-boxplot(excitement[["excite_score"]] ~ excitement[[i]], data = excitement,
+boxplot(excitement_joined[["excite_score"]] ~ excitement_joined[["is_thurs"]], data = excitement_joined,
         xlab="Thursday/Not Thursday", ylab="excitement score",
         main = "Score Distributions on Thursdays and Non-Thursdays")

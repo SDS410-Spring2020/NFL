62f69cee75a3424e5a82259149a1e66e04105e4f
aballou16 <aballou@smith.edu>
2020-02-18 14:05:25 -0500

    added a ggplot for comparing excitement

--- excitement_thurs.R
+++ excitement_thurs.R
@@ -1,13 +1,9 @@
-#Use the games ds (thursday = binary)
-head(games)
+#Use the fulldata_with_thurs ds (thursday = binary)
+head(fulldata_with_thurs)
 
 #reading in the two nfl data frames 
 games<- read.csv("data/games_Smith.csv")
 pbp<- read.csv("data/pbp_Smith.csv")
-
-#creating binary thursday in games df
-thurs_games <- fulldata_with_thurs %>% 
-  mutate(is_thurs = ifelse(Game_Day == "Thursday", 1, 0))
 
 #create an excitement variable
 #using thurs_games data
@@ -17,11 +13,13 @@ excitement <- fulldata_with_thurs %>%
   
 #join excitement score with thurs_games ds 
 excitement_joined <- excitement %>% 
-  full_join(thurs_games, by = "GameKey")
+  full_join(fulldata_with_thurs, by = "GameKey")
 
-thurs_excite_box <- ggplot(data = excitement_joined, aes(y = rowSums, group = as.factor(have_arthritis), color = as.factor(have_arthritis))) +
+#look at exctiment score on Thurs vs Not Thurs using ggplot
+thurs_excite_box <- ggplot(data = excitement_joined, aes(y = excite_score, group = as.factor(is_thurs), color = as.factor(is_thurs))) +
   geom_boxplot() +
-  ggtitle("Side by Side Box plots using rowSums ~ Gender")
+  ggtitle("Comparing excitement on Thurs. vs. not Thurs games")
+thurs_excite_box
 
 #look at excitement score on Thurs vs Not Thurs
 boxplot(excitement_joined[["excite_score"]] ~ excitement_joined[["is_thurs"]], data = excitement_joined,

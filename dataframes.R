7a28eb2b959b568995933ff199635762b05acc1c
Lizette <lcarpenter@smith.edu>
2020-03-10 13:33:14 -0400

    cleaned games and pbp

--- dataframes.R
+++ dataframes.R
@@ -1,78 +1,96 @@
 #games
-games<-read.csv("data/games_Smith.csv")
-
+games <-read.csv("data/games_Smith.csv") %>% 
+  clean_names() %>%
+  mutate(game_date = str_replace(game_date, "T00:00:00Z", ""),
+         game_date = as.Date(game_date, "%Y-%m-%d")) 
 #pbp
-pbp<-read.csv("data/pbp_Smith.csv")
+pbp<-read.csv("data/pbp_Smith.csv") %>% 
+  clean_names() %>% 
+  mutate(
+    game_clock = as.numeric(as.period(ms(game_clock), unit = "sec")),
+    game_clock = 
+      case_when(quarter == 1 ~ game_clock,
+                quarter == 2 ~ game_clock + 900,
+                quarter == 3 ~ game_clock + 1800,
+                quarter == 4 ~ game_clock + 2700,
+                quarter == 5 ~ game_clock + 3600))
 
 #data (combined)
-data<-games %>% 
-  left_join(pbp, by="GameKey")
+data <- games %>% 
+  left_join(pbp, by="game_key")
 #note: NAs for 2007-2010 in pbp
 
 #fulldata_with_thurs 
 fulldata_with_thurs <- data %>% 
-  mutate(is_thurs = ifelse(Game_Day == "Thursday", 1, 0))
+  mutate(is_thurs = ifelse(game_day == "Thursday", 1, 0))
 
 #nfl_elo_2019 (cleaned)
-nfl_elo_2019 <- read.csv("nfl_elo_latest.csv")
-nfl_elo_2019 <- nfl_elo_2019 %>% 
-  rename(HomeClubCode = team1, 
-         VisitorClubCode = team2)
+nfl_elo_2019 <- read.csv("nfl_elo_latest.csv") %>% 
+  clean_names() %>% 
+  rename(
+    home_club_code = team1, 
+    visitor_club_code = team2)
   mutate(
-    date = as.Date(date, "%Y-%m-%d"), 
-    HomeClubCode = 
+    game_date = as.Date(date, "%Y-%m-%d"), 
+    home_club_code = 
       case_when(
-        as.character(HomeClubCode) == "HOU" ~ "HST",
-        as.character(HomeClubCode) == "LAR" ~ "SL",
-        as.character(HomeClubCode) == "WSH" ~ "WAS", 
-        as.character(HomeClubCode) == "CLE" ~ "CLV",
-        as.character(HomeClubCode) == "BAL" ~ "BLT",
-        as.character(HomeClubCode) == "ARI" ~ "ARZ",
-        as.character(HomeClubCode) == "LAC" & as.character(season) >= "2017" ~ "SD",
-        as.character(HomeClubCode) == "LAC" & as.character(season) <= "2016" ~ "LA",
-        TRUE ~ as.character(HomeClubCode)), 
-    VisitorClubCode = 
+        as.character(home_club_code) == "HOU" ~ "HST",
+        as.character(home_club_code) == "LAR" ~ "SL",
+        as.character(home_club_code) == "WSH" ~ "WAS", 
+        as.character(home_club_code) == "CLE" ~ "CLV",
+        as.character(home_club_code) == "BAL" ~ "BLT",
+        as.character(home_club_code) == "ARI" ~ "ARZ",
+        as.character(home_club_code) == "LAC" & as.character(season) >= "2017" ~ "SD",
+        as.character(home_club_code) == "LAC" & as.character(season) <= "2016" ~ "LA",
+        TRUE ~ as.character(home_club_code)), 
+    visitor_club_code = 
       case_when(
-        as.character(VisitorClubCode) == "HOU" ~ "HST",
-        as.character(VisitorClubCode) == "LAR" ~ "SL",
-        as.character(VisitorClubCode) == "WSH" ~ "WAS", 
-        as.character(VisitorClubCode) == "CLE" ~ "CLV",
-        as.character(VisitorClubCode) == "BAL" ~ "BLT",
-        as.character(VisitorClubCode) == "ARI" ~ "ARZ",
-        as.character(VisitorClubCode) == "LAC" & as.character(season) >= "2017" ~ "SD",
-        as.character(VisitorClubCode) == "LAC" & as.character(season) <= "2016" ~ "LA",
-        TRUE ~ as.character(VisitorClubCode)))
+        as.character(visitor_club_code) == "HOU" ~ "HST",
+        as.character(visitor_club_code) == "LAR" ~ "SL",
+        as.character(visitor_club_code) == "WSH" ~ "WAS", 
+        as.character(visitor_club_code) == "CLE" ~ "CLV",
+        as.character(visitor_club_code) == "BAL" ~ "BLT",
+        as.character(visitor_club_code) == "ARI" ~ "ARZ",
+        as.character(visitor_club_code) == "LAC" & as.character(season) >= "2017" ~ "SD",
+        as.character(visitor_club_code) == "LAC" & as.character(season) <= "2016" ~ "LA",
+        TRUE ~ as.character(visitor_club_code)))
 
 #nfl_elo (cleaned)
-nfl_elo <- read.csv("nfl_elo.csv")
-nfl_elo_ <- nfl_elo %>% 
-  rename(HomeClubCode = team1, 
-         VisitorClubCode = team2) %>% 
+nfl_elo <- read.csv("nfl_elo.csv") %>% 
+  clean_names() %>% 
+  rename(
+    home_club_code = team1, 
+    visitor_club_code = team2) %>% 
   mutate(
-    date = as.Date(date, "%Y-%m-%d"), 
-    HomeClubCode = 
+    game_date = as.Date(date, "%Y-%m-%d"), 
+    home_club_code = 
       case_when(
-        as.character(HomeClubCode) == "HOU" ~ "HST",
-        as.character(HomeClubCode) == "LAR" ~ "SL",
-        as.character(HomeClubCode) == "WSH" ~ "WAS", 
-        as.character(HomeClubCode) == "CLE" ~ "CLV",
-        as.character(HomeClubCode) == "BAL" ~ "BLT",
-        as.character(HomeClubCode) == "ARI" ~ "ARZ",
-        as.character(HomeClubCode) == "LAC" & as.character(season) >= "2017" ~ "SD",
-        as.character(HomeClubCode) == "LAC" & as.character(season) <= "2016" ~ "LA",
-        TRUE ~ as.character(HomeClubCode)), 
-    VisitorClubCode = 
+        as.character(home_club_code) == "HOU" ~ "HST",
+        as.character(home_club_code) == "LAR" ~ "SL",
+        as.character(home_club_code) == "WSH" ~ "WAS", 
+        as.character(home_club_code) == "CLE" ~ "CLV",
+        as.character(home_club_code) == "BAL" ~ "BLT",
+        as.character(home_club_code) == "ARI" ~ "ARZ",
+        as.character(home_club_code) == "LAC" & as.character(season) >= "2017" ~ "SD",
+        as.character(home_club_code) == "LAC" & as.character(season) <= "2016" ~ "LA",
+        TRUE ~ as.character(home_club_code)), 
+    visitor_club_code = 
       case_when(
-        as.character(VisitorClubCode) == "HOU" ~ "HST",
-        as.character(VisitorClubCode) == "LAR" ~ "SL",
-        as.character(VisitorClubCode) == "WSH" ~ "WAS", 
-        as.character(VisitorClubCode) == "CLE" ~ "CLV",
-        as.character(VisitorClubCode) == "BAL" ~ "BLT",
-        as.character(VisitorClubCode) == "ARI" ~ "ARZ",
-        as.character(VisitorClubCode) == "LAC" & as.character(season) >= "2017" ~ "SD",
-        as.character(VisitorClubCode) == "LAC" & as.character(season) <= "2016" ~ "LA",
-        TRUE ~ as.character(VisitorClubCode)))
+        as.character(visitor_club_code) == "HOU" ~ "HST",
+        as.character(visitor_club_code) == "LAR" ~ "SL",
+        as.character(visitor_club_code) == "WSH" ~ "WAS", 
+        as.character(visitor_club_code) == "CLE" ~ "CLV",
+        as.character(visitor_club_code) == "BAL" ~ "BLT",
+        as.character(visitor_club_code) == "ARI" ~ "ARZ",
+        as.character(visitor_club_code) == "LAC" & as.character(season) >= "2017" ~ "SD",
+        as.character(visitor_club_code) == "LAC" & as.character(season) <= "2016" ~ "LA",
+        TRUE ~ as.character(visitor_club_code)))
 
+#full_with_elo 
+full_with_elo= merge(nfl_elo, 
+                     fulldata_with_thurs, 
+                     by.x=c("home_club_code", "visitor_club_code", "game_date"), 
+                     by.y=c("home_club_code", "visitor_club_code", "game_date"))
 #NFL_team_city
 
 #Stadium_data
